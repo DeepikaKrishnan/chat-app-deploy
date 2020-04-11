1a694e6a357fa9dc3aedc775e5d5042eb96c1991
Deepika <deepika@isquarebs.in>
2020-04-11 12:55:56 +0530

    added pwa logic build

--- index.php
+++ index.php
@@ -6,8 +6,11 @@
   <base href="/">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="icon" type="image/x-icon" href="favicon.ico">
+  <link rel="manifest" href="manifest.webmanifest">
+  <meta name="theme-color" content="#1976d2">
 <link rel="stylesheet" href="styles.8205ac0d3c3d293905a9.css"></head>
 <body>
   <app-root></app-root>
-<script src="runtime-es2015.c9afb3256f2870e161de.js" type="module"></script><script src="runtime-es5.c9afb3256f2870e161de.js" nomodule defer></script><script src="polyfills-es5.0e4e1968447fab48e788.js" nomodule defer></script><script src="polyfills-es2015.bbb42ff2e1c488ff52d5.js" type="module"></script><script src="scripts.f6fa858fac9509d4d711.js" defer></script><script src="main-es2015.2f930cdbbe28fc778a10.js" type="module"></script><script src="main-es5.2f930cdbbe28fc778a10.js" nomodule defer></script></body>
+  <noscript>Please enable JavaScript to continue using this application.</noscript>
+<script src="runtime-es2015.c9afb3256f2870e161de.js" type="module"></script><script src="runtime-es5.c9afb3256f2870e161de.js" nomodule defer></script><script src="polyfills-es5.0e4e1968447fab48e788.js" nomodule defer></script><script src="polyfills-es2015.bbb42ff2e1c488ff52d5.js" type="module"></script><script src="scripts.f6fa858fac9509d4d711.js" defer></script><script src="main-es2015.d8cb30851c4773d5597c.js" type="module"></script><script src="main-es5.d8cb30851c4773d5597c.js" nomodule defer></script></body>
 </html>
